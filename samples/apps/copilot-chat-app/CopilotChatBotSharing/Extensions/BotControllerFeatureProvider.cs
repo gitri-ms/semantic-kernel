191d4914327ab7c2fe11d072eb51e330e8b3fe50
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-06-08 17:07:12 -0700

    Begin splitting out BotSharing code into separate lib -- DOES NOT BUILD

---
+++ samples/apps/copilot-chat-app/CopilotChatBotSharing/Extensions/BotControllerFeatureProvider.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using SemanticKernel.Service.CopilotChat.BotSharing.Controllers;
+
+namespace SemanticKernel.Service.CopilotChat.BotSharing.Extensions;
+
+public class BotControllerFeatureProvider : CustomControllerFeatureProvider
+{
+    public BotControllerFeatureProvider() : base(new List<Type> { typeof(BotController) }) { }
+}
---
+++ samples/apps/copilot-chat-app/CopilotChatBotSharing/Extensions/MvcBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SemanticKernel.Service.CopilotChat.BotSharing.Extensions;
+
+public static class MvcBuilderExtensions
+{
+    public static IMvcBuilder AddCopilotChatBotSharing(this IMvcBuilder builder)
+    {
+        // Enable the bot controller
+        builder.ConfigureApplicationPartManager(mgr =>
+        {
+            mgr.FeatureProviders.Add(new BotControllerFeatureProvider());
+        });
+        return builder;
+    }
+
+    // Hypothetical "bot sharing" nuget could include Models/Bot.cs, Controllers/BotController.cs,
+    // the BotControllerFeatureProvider class, and the AddCopilotChatBotSharing extension above.
+
+}
